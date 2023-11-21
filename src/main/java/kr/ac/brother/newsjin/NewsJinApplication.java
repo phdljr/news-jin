2cf95750008146f8819e433d84e6d98f1674ffca
phdljr <phdljr@naver.com>
2023-11-21 19:34:49 +0900

    init project

---
+++ src/main/java/kr/ac/brother/newsjin/NewsJinApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.brother.newsjin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsJinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsJinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/brother/newsjin/NewsJinApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.brother.newsjin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsJinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
